9974e2d667f473d4a70fda5f6e51033806ac5a66
sohail.ansari <sohail.ansari@transneuron.com>
2025-05-24 01:53:32 +0530

    SIDH-93709 - 12 files changed - completed UI

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,25 @@ import { EmailService } from "./shared/email/email.service";
 const app = new Hono();
 
 app.notFound((c) => {
-  return c.text('404-NotFound'          , 404);
+	return c.text("404-NotFound", 404);
 });
 app.onError((err, c) => {
-  console.error(`${err}`);
-  return c.text(`${err}`, 500);
+	console.error(`${err}`);
+	return c.text(`${err}`, 500);
 });
 
 app.get("/", async (c) => {
-  try {
-    const es = EmailService.instance;
-    await es.sendEmail({
-      subject: "test",
-      to: "syansari02@gmail.com",
-      html: "hi",
-    });
-    return c.text(`Hello Hono! ${Bun.env.PORT}`);
-  } catch (error) {
-    return c.text(`${error}`, 500);
-  }
+	try {
+		const es = EmailService.instance;
+		await es.sendEmail({
+			subject: "test",
+			to: "syansari02@gmail.com",
+			html: "hi",
+		});
+		return c.text(`Hello Hono! ${Bun.env.PORT}`);
+	} catch (error) {
+		return c.text(`${error}`, 500);
+	}
 });
 
 export default app;
