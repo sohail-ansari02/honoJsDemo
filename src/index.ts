33aeb5ac71ef53d2fbe281c903ddb923c300d823
sohail.ansari <sohail.ansari@transneuron.com>
2025-05-23 23:05:54 +0530

    SIDH-93709 - 12 files changed - completed UI

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-	return c.text("Hello Hono!");
+	return c.text(`Hello Hono! ${Bun.env.PORTT}`);
 });
 
 // Simple recursive Fibonacci (not efficient for large numbers)
