467d9bcf3d0018b200359bf480da0a54caa6b857
sohail.ansari <sohail.ansari@transneuron.com>
2025-05-25 18:44:35 +0530

    added file util

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { EmailService } from "@shared/email/email.service";
 import { TemplateService } from "@shared/template/template.service";
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono();
 
@@ -8,8 +8,11 @@ app.notFound((c) => {
 	return c.text("404-NotFound", 404);
 });
 app.onError((err, c) => {
-	console.error(err);
-	return c.text(`${err}`, 500);
+	console.log(err);
+	if (err instanceof HTTPException) {
+		return c.text(` ${err.status} - custom err-  ${err}`, err.status);
+	}
+	return c.text("Internal Server Error", 500);
 });
 
 app.get("/", async (c) => {
@@ -17,7 +20,9 @@ app.get("/", async (c) => {
 		const res = await TemplateService.instance.render("demo", { name: "mj" });
 		return c.html(`Hello Hono! <br/> ${res}`);
 	} catch (error) {
-		return c.text(`${error}`, 500);
+		throw new HTTPException(500, {
+			message: error instanceof Error ? error.message : "Unknown error",
+		});
 	}
 });
 
