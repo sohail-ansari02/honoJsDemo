86f2a26598b8c9e17488e46b9a35d5b5afa8525a
sohail.ansari <sohail.ansari@transneuron.com>
2025-06-04 10:19:20 +0530

    .

--- src/config/constants/template.const.ts
+++ src/config/constants/template.const.ts
@@ -1,4 +1,8 @@
 const TEMPLATES_PATH = `${process.cwd()}/public/templates`;
+
+export const TEMPLATE_FILE_EXT = "mustache";
+export const TEMPLATE_TYPES = ["email", "html"] as const;
+
 export const EMAIL_TEMPLATES_PATH = `${TEMPLATES_PATH}/email`;
 export const HTML_TEMPLATES_PATH = `${TEMPLATES_PATH}/html`;
 
@@ -6,5 +10,3 @@ export const EMAIL_TEMPLATE_NAMES = [
 	"order-recieved",
 	"order-confirmation",
 ] as const;
-export const TEMPLATE_FILE_EXT = "mustache";
-export const TEMPLATE_TYPES = ["email", "html"] as const;
